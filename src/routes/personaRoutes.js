7413ec01ee655f223f54ceb15a9b3a1a495ea037
김서연 <seoyoun1004@gmail.com>
2025-07-16 23:36:31 +0900

    test5?-idk

---
+++ src/routes/personaRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const personaController = require('../controllers/personaController');
+
+/**
+ * @swagger
+ * /personas/existing:
+ *   post:
+ *     summary: 캐릭터 생성
+ *     tags: [Persona]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               image_url:
+ *                 type: string
+ *               clerk_id:
+ *                 type: string
+ *               is_public:
+ *                 type: boolean
+ *             required:
+ *               - name
+ *               - image_url
+ *               - clerk_id
+ *               - is_public
+ *     responses:
+ *       201:
+ *         description: 캐릭터 생성 성공
+ *       400:
+ *         description: 캐릭터 생성 실패
+ */
+router.post('/existing', personaController.createPersona);
+
+module.exports = router;
--- src/services/characterService.js
+++
@@ -1,9 +0,0 @@
-const prisma = require('../config/prisma');
-
-async function createPersona({ clerk_id, name, image_url, is_public }) {
-  return prisma.persona.create({
-    data: { clerk_id, name, image_url, is_public }
-  });
-}
-
-module.exports = { createPersona };
