3b4bf91311488570d78d106c9ec785620360da5e
dhdid <lleewnrhkd@gmail.com>
2025-07-17 11:08:41 +0900

    수정

--- src/routes/communities/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const charactersRouter = require('./characters');
-router.use('/characters', charactersRouter);
-
-module.exports = router;
