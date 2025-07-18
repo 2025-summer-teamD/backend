00d543f4d57e61aa6a8b7491e98eb2392a7b8e8d
Baikhyunjun <efc3825@konkuk.ac.kr>
2025-07-19 00:13:51 +0900

    chat

--- src/routes/_index.js
+++ src/routes/_index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import communitiRouter from './communitiRoutes.js';
 import personaRouter from './personaRoute.js';
 import userRouter from './userRoute.js';
+import chatRouter from './chatRoutes.js';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.use('/communities', communitiRouter);
 router.use('/my', userRouter);
 router.use('/characters', personaRouter);
+router.use('/chat', chatRouter);
 
 export default router;
