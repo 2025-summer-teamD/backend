7863007543f96fabcda91b4e4e31477c66f2f8e5
tutletutley <mg7694@naver.com>
2025-07-29 00:41:07 +0900

    수정

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -30,8 +30,7 @@ const elevenlabs = new ElevenLabsClient({
  * @returns {number} 이모지 개수
  */
 const countEmojis = (text) => {
-  // 이모지 정규식 (유니코드 이모지 범위)
-  const emojiRegex = /[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F900}-\u{1F9FF}]|[\u{1F018}-\u{1F270}]|[\u{238C}-\u{2454}]|[\u{20D0}-\u{20FF}]|[\u{FE00}-\u{FE0F}]|[\u{1FAB0}-\u{1FABF}]|[\u{1FAC0}-\u{1FAFF}]|[\u{1FAD0}-\u{1FAFF}]|[\u{1FAE0}-\u{1FAFF}]|[\u{1FAF0}-\u{1FAFF}]/gu;
+  const emojiRegex = /\p{Emoji}/gu;  // ES2018+ 유니코드 이모지 프로퍼티 사용
   const matches = text.match(emojiRegex);
   return matches ? matches.length : 0;
 };
